33ffab03cd8745d8b8b93e1537e74a043f19298d
adxxya30 <adityaofficial714@gmail.com>
2026-02-03 18:32:56 +0700

    feat(nvim): switch to LazyVim with Catppuccin & Yazi
    - Replace NvChad with LazyVim starter
    - Move config to dotfiles and symlink
    - Add plugins/theme.lua: Catppuccin Mocha + Transparency
    - Add plugins/yazi.lua: Yazi integration (<leader>-)

---
+++ home/.config/nvim/lua/plugins/yazi.lua
@@ -0,0 +1,25 @@
+return {
+  "mikavilpas/yazi.nvim",
+  event = "VeryLazy",
+  keys = {
+    -- 👇 in this section, choose your own keymappings!
+    {
+      "<leader>-",
+      "<cmd>Yazi<cr>",
+      desc = "Open Yazi",
+    },
+    {
+      -- Open in the current working directory
+      "<leader>cw",
+      "<cmd>Yazi cwd<cr>",
+      desc = "Open Yazi in CWD",
+    },
+  },
+  opts = {
+    -- if you want to open yazi instead of netrw, see below for more info
+    open_for_directories = false,
+    keymaps = {
+      show_help = '<f1>',
+    },
+  },
+}
